6d98d37514ec073625b6cff60b13eb48d662505a
Anushay Furqan <anushay.furqan@gmail.com>
2017-03-07 16:52:48 -0500

    Level select screen opens corresponding scenes

---
+++ Assets/scripts/loadlevel1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+	public class loadlevel1 : MonoBehaviour {
+		
+	public Button level1;
+
+	public void Start () {
+		level1 = level1.GetComponent<Button> ();
+	}
+
+	public void nextlevel () {
+		SceneManager.LoadScene("level 1");
+	}
+}
+
