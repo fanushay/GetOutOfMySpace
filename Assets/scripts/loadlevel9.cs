8c3c551a8681a6d1d0f1b1f6e6f8bee553f119e8
Kristin DeChiaro <kmd427@drexel.edu>
2017-03-13 19:57:54 -0400

    added level 9 and 10. take a look when you can

---
+++ Assets/scripts/loadlevel9.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class loadlevel9 : MonoBehaviour {
+
+	public Button level9;
+
+	public void Start () {
+		level9 = level9.GetComponent<Button> ();
+	}
+
+	public void nextlevel () {
+		SceneManager.LoadScene("level 9");
+	}
+}
+
